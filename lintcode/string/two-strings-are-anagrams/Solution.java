9962d00e7802f28a4d5adc0340b35ff24f1ad2e4
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-06-01 09:48:38 +0800

    Create Solution.java

---
+++ lintcode/string/two-strings-are-anagrams/Solution.java
@@ -0,0 +1,26 @@
+// http://www.lintcode.com/en/problem/two-strings-are-anagrams/
+
+public class Solution {
+    /**
+     * @param s: The first string
+     * @param b: The second string
+     * @return true or false
+     */
+    public boolean anagram(String s, String t) {
+        if (s.length() != t.length()) {
+           return false;
+        }
+        
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            count[(int) s.charAt(i)]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            count[(int) t.charAt(i)]--;
+            if (count[(int) t.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

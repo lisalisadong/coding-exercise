9f321eb86a55b6490f5b6d29656e1a96aebd49b4
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-06-01 09:35:43 +0800

    Create Solution.java

---
+++ leetcode/containsDuplicate/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numbers = new HashSet<Integer>();
+        for (int number : nums) {
+            if (numbers.contains(number)) {
+                return true;
+            } else {
+                numbers.add(number);
+            }
+        }
+        return false;
+    }
+}

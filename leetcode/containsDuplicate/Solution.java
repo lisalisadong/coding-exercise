f2f465290c9ec1a840469b90255371a6778f9ed6
Qingxiao Dong <qingxiao.dong@gmail.com>
2015-06-01 09:42:38 +0800

    Update Solution.java

--- leetcode/containsDuplicate/Solution.java
+++ leetcode/containsDuplicate/Solution.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/contains-duplicate/
+
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> numbers = new HashSet<Integer>();
